497e1f24724a120e0d4f53f60af0962240e20fe7
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2023-08-16 09:10:35 +0200

    Version bump: 1.0.5 ScopeVlp mapping

---
+++ lib/libis/metadata/mappers/scope_vlp.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+require 'libis/metadata/dublin_core_record'
+require 'libis/tools/assert'
+
+module Libis
+  module Metadata
+    module Mappers
+      # noinspection RubyResolve
+
+      # Mixin for {::Libis::Metadata::DublinCoreRecord} to enable conversion of the Scope exported DC record.
+      module ScopeVlp
+
+        # Main conversion method.
+        # @return [::Libis::Metadata::DublinCoreRecord]
+        def to_dc
+          assert(self.is_a? Libis::Metadata::DublinCoreRecord)
+
+          doc = Libis::Metadata::DublinCoreRecord.new(self.to_xml)
+
+          # set isPartOf
+          doc.isPartOf = doc.title.content
+
+          # remove identifiers with URL
+          doc.xpath('//dc:identifier[@xsi:type = "dcterms:URI"]').remove
+
+          doc
+        end
+
+      end
+
+    end
+  end
+end
